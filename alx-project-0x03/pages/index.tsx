b5a57e394a53fa19c8bef34856d855eda72196c4
Jana-Ruth <ruthewenode73@gmail.com>
2025-07-25 08:14:05 +0100

    add ;

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -4,7 +4,7 @@ import router, { useRouter } from "next/router";
 import { PageRouteProps } from "@/interface";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   // Imeperative routing with useRouter
   const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
